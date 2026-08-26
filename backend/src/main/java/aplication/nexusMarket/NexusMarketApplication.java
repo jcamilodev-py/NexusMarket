e63f9c826b86dc8c260d3bc00ae898f8f433447a
pyjuan001 <juancamilosernacortes12345@gmail.com>
2026-08-26 14:27:52 -0500

    spring initializr

---
+++ backend/src/main/java/aplication/nexusMarket/NexusMarketApplication.java
@@ -0,0 +1,13 @@
+package aplication.nexusMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexusMarketApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/aplication/nexusMarket/NexusMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package aplication.nexusMarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexusMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
